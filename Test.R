a6078aba65905c8a0f8df94ffbce80b4b7b52164
EaglesFans <choonghoon.hyun@gmail.com>
2021-12-05 13:13:02 -0500

    Yes I did it.

--- Test.R
+++ Test.R
@@ -1,3 +1,4 @@
 
-## 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
+## Use this token for your credential: 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
 print("This file was created within RStudio1")
+print("Yes I did it.")
